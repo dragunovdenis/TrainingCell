54111617677c6022a5cfbe7a6d40a81113d19f29
Denys_Dragunov <dragunovdenis@gmail.com>
2023-05-05 21:34:25 +0300

    Optimization of TdLambdaAgent and update of the corresponding regression test

--- TrainingCell/Source/Checkers/TdLambdaAgent.cpp
+++ TrainingCell/Source/Checkers/TdLambdaAgent.cpp
@@ -58,7 +58,7 @@ namespace TrainingCell::Checkers
 
 			for (auto layer_id = 0ull; layer_id < gradient.size(); ++layer_id)
 			{
-				gradient[layer_id] += _z[layer_id] * _lambda * _gamma;
+				gradient[layer_id].add_scaled(_z[layer_id], _lambda * _gamma);
 				_z[layer_id] = std::move(gradient[layer_id]);
 			}
 		}
